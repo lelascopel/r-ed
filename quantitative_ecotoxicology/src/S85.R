4650eb16e8614b9055e02e9252519ba7ff62c035
EDiLD <szoe8822@uni-landau.de>
2012-09-13 19:30:02 +0200

    data & code

---
+++ quantitative_ecotoxicology/src/S85.R
@@ -0,0 +1,44 @@
+OYSTERZN<- read.table(file.path(getwd(), "quantitative_ecotoxicology/data/S85.csv"), 
+                      header = TRUE, 
+                      sep = ";")
+
+OYSTERZN$WNLIN <- OYSTERZN$DAY^2
+OYSTERZN$LZINC <- log(OYSTERZN$ZINC)
+plot(ZINC ~ DAY, data = OYSTERZN)
+
+# without weights
+mod <- nls(ZINC ~ INITACT * exp((-(KE+0.00283))*DAY), OYSTERZN,
+           start = list(KE=0.004, INITACT = 500))
+summary(mod)
+pred_mod <- predict(mod)
+res_mod <- resid(mod)
+# Residuals
+plot(OYSTERZN$DAY, res_mod)
+abline(h = 0)
+
+
+# with fixed weights
+mod2 <- nls(ZINC ~ INITACT * exp((-(KE+0.00283))*DAY), OYSTERZN,
+            weights=OYSTERZN$WNLIN, 
+            start = list(KE=0.004, INITACT = 500))
+summary(mod2)
+pred_mod2 <- predict(mod2)
+
+# Data + fitted
+plot(ZINC ~ DAY, data = OYSTERZN)
+lines(OYSTERZN$DAY, pred_mod)
+lines(OYSTERZN$DAY, pred_mod2, lty = "dashed")
+
+
+
+# Lm
+mod3 <- lm(LZINC ~ DAY, data = OYSTERZN)
+summary(mod3)
+pred_mod3 <- predict(mod3)
+
+# data + fitted
+plot(LZINC ~ DAY, OYSTERZN)
+lines(OYSTERZN$DAY, pred_mod3)
+
+# residuals
+plot(mod3, which = 1)
