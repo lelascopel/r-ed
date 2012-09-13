9b539a9ed2662d03216bb74e538fce5e393d0ec1
EDiLD <szoe8822@uni-landau.de>
2012-09-13 19:38:07 +0200

    data & code

---
+++ quantitative_ecotoxicology/src/S94.R
@@ -0,0 +1,11 @@
+LEAD <- read.table(file.path(getwd(), "quantitative_ecotoxicology/data/S94.csv"), 
+                      header = TRUE, 
+                      sep = ";")
+
+LEAD$LLEAD <- log(LEAD$LEAD)
+LEAD$LDAY <- log(LEAD$DAY)
+
+mod <- lm(LLEAD ~ LDAY, data = LEAD)
+plot(LLEAD ~ LDAY, data = LEAD)
+abline(mod)
+summary(mod)
---
+++ quantitative_ecotoxicology/src/convert.R
@@ -0,0 +1,35 @@
+read_new <- function(x){
+  con <- file(x)
+  tmp <- readLines(con) # Read one line 
+  close(con)
+  
+  require(stringr)
+  var <- strsplit(tmp[1], split = " ")
+  var_a <- which(var[[1]] == "@@;")
+  vars <- var[[1]][2:(var_a - 1)]
+  nvar <- length(vars)
+  
+  data_start <- grep("DATALINES;", tmp)
+  data <- tmp[(data_start + 1):(length(tmp)-1)]
+  data <- paste(data, collapse= " ")
+  data <- unlist(strsplit(data, split=" "))
+  
+  out <- list()
+  for(i in seq_along(vars)){
+    se <- 1:nvar
+    take <- rep(se, times = length(data) / nvar)    
+    out[[vars[i]]] <- data[take == i]
+  }
+  out <- data.frame(out)
+  out
+}
+
+setwd("/home/edisz/Downloads/Newman_data")
+files <- list.files(file.path(getwd(), "data"), full.names=TRUE, pattern="*.txt")
+for(i in seq_along(files)){
+  print(files[i])
+  df <- read_new(files[i])
+  write.table(df, gsub(".txt", ".csv", files[i]), row.names=FALSE, sep = ";")
+}
+
+
